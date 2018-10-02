cee0eff79917ce62cc988cdc9c32b72aa84626c5
jicardu <ricardosantamaria@outlook.com>
2018-10-02 09:17:10 -0500

    Encrypt password, log db errors and send responses

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,17 +1,23 @@
 const User = require('../models').User;
+const bcrypt = require('bcrypt');
+const logger = require('../logger');
+
+const saltRounds = 10;
 
 exports.signup = function(req, res) {
   const { name, lastname, email, password } = req.body;
-  User.create({ name, lastname, email, password })
-    .then(user => {
-      res.status(200);
-      res.end();
-    })
-    .catch(error => {
-      res.status(500);
-    });
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    User.create({ name, lastname, email, password: hash })
+      .then(user => {
+        logger.info(`User ${name} created.`);
+        res.status(200).json({ user });
+        res.end();
+      })
+      .catch(error => {
+        logger.error(error);
+        res.statusMessage = error;
+        res.status(500).json({ error });
+        res.end();
+      });
+  });
 };
-
-// Las contraseñas no deben guardarse de manera plana.
-// Se debe loggear un mensaje con el nombre de usuario cuando éste se haya creado correctamente, y uno de error en caso contrario.
-// Loggear un error en caso de falla de la base de datos.
