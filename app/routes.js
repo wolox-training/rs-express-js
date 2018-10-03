f2e27100667eccda262084ea6e701c2efb8e485c
jicardu <ricardosantamaria@outlook.com>
2018-10-03 10:08:25 -0500

    Add validation for required fields at request level

--- app/routes.js
+++ app/routes.js
@@ -5,19 +5,31 @@ const { check, validationResult } = require('express-validator/check');
 exports.init = app => {
   app.post(
     '/users',
-    check('email').matches(
-      /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(wolox)\.(com|co|com\.ar)$/
-    ),
-    check('email').custom(value => {
-      return User.findOne({ where: { email: value } }).then(user => {
-        if (user) {
-          return Promise.reject('E-mail already in use');
-        }
-      });
-    }),
+    check('name')
+      .exists()
+      .withMessage('Name is required'),
+    check('lastname')
+      .exists()
+      .withMessage('Lastname is required'),
+    check('email')
+      .exists()
+      .withMessage('E-mail is required')
+      .matches(/^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(wolox)\.(com|co|com\.ar)$/)
+      .withMessage('E-mail must be from wolox domain')
+      .custom(value => {
+        return User.findOne({ where: { email: value } }).then(user => {
+          if (user) {
+            return Promise.reject('E-mail already in use');
+          }
+        });
+      }),
     check('password')
+      .exists()
+      .withMessage('Password is required')
       .isLength({ min: 8 })
-      .matches(/^[a-zA-Z0-9]*$/),
+      .withMessage('Password should be 8 characters minimum')
+      .matches(/^[a-zA-Z0-9]*$/)
+      .withMessage('Password should be alphanumeric only'),
     function(req, res, next) {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -27,7 +39,5 @@ exports.init = app => {
       }
     },
     userController.signup
-    // app.put('/endpoint/put/path', [], controller.methodPUT);
-    // app.post('/endpoint/post/path', [], controller.methodPOST);
   );
 };
