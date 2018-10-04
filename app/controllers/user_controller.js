e9f843de906471d193cd3c50f246f6ae7cc12afe
jicardu <ricardosantamaria@outlook.com>
2018-10-04 10:00:23 -0500

    Move password encryptation from controller to model

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,23 +1,18 @@
-const User = require('../models').User;
-const bcrypt = require('bcrypt');
-const logger = require('../logger');
-
-const saltRounds = 10;
+const User = require('../models').User,
+  logger = require('../logger');
 
 exports.signup = function(req, res) {
   const { name, lastname, email, password } = req.body;
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-    User.create({ name, lastname, email, password: hash })
-      .then(user => {
-        logger.info(`User ${name} created.`);
-        res.status(200).json({ user });
-        res.end();
-      })
-      .catch(error => {
-        logger.error(error);
-        res.statusMessage = error;
-        res.status(500).json({ error });
-        res.end();
-      });
-  });
+  User.create({ name, lastname, email, password })
+    .then(user => {
+      logger.info(`User ${name} created.`);
+      res.status(201).json({ user });
+      res.end();
+    })
+    .catch(error => {
+      logger.error(error);
+      res.statusMessage = error;
+      res.status(500).json({ error });
+      res.end();
+    });
 };
--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
@@ -16,10 +18,21 @@ module.exports = (sequelize, DataTypes) => {
       },
       password: { type: DataTypes.STRING, allowNull: false }
     },
-    {}
+    {
+      instanceMethods: {
+        validPassword(password) {
+          return bcrypt.compare(password, this.password);
+        }
+      }
+    }
   );
   User.associate = function(models) {
     // associations can be defined here
   };
+  User.beforeCreate((user, options) => {
+    return bcrypt.hash(user.password, 10).then(hash => {
+      user.password = hash;
+    });
+  });
   return User;
 };
